83f53889d47f960c15eb6c5f55f8a592f77cee65
Alejo Madrid Giral <alejomadridg@hotmail.com>
2024-11-30 10:03:05 -0500

    first component and services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntrepreneurshipComponent } from './components/entrepreneurship/entrepreneurship.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"" , component : EntrepreneurshipComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/entrepreneurship/entrepreneurship.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entrepreneurship',
+  standalone: false,
+  
+  templateUrl: './entrepreneurship.component.html',
+  styleUrl: './entrepreneurship.component.css'
+})
+export class EntrepreneurshipComponent {
+
+}
