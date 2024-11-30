7aa7815916c1d4ab76f5e5079678e319e783bb7d
Alejo Madrid Giral <alejomadridg@hotmail.com>
2024-11-30 10:12:06 -0500

    service creation for entrepreneurship

--- src/app/services/entrepreneurship.service.ts
+++ src/app/services/entrepreneurship.service.ts
@@ -1,9 +1,52 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Entrepreneurship{
+  idEntrepreneurship: number;
+  entrepreneurshipName: string; 
+  entrepreneurshipDescription: string;
+  image: string;
+  address: string;
+  idCity: number;
+  idUser: number;
+  idCategories: number[]
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EntrepreneurshipService {
+  private apiUrl ="http://localhost:8080/entrepreneurship";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+    //metodos
+
+    getEntrepreneurship():Observable<Entrepreneurship[]>{
+      return this.http.get<Entrepreneurship[]>(this.apiUrl)
+    }
+
+     // Método para obtner un Entrepreneurship por id
+     getIdEntrepreneurship(idEntrepreneurship: number): Observable<Entrepreneurship> {
+      // Se hace una solicitud GET para obtener Entrepreneurship por su id
+      return this.http.get<Entrepreneurship>(`${this.apiUrl}/${idEntrepreneurship}`);
+    }
+  
+    createEntrepreneurship(entrepreneurship: Entrepreneurship):Observable<Entrepreneurship>{
+      return this.http.post<Entrepreneurship>(this.apiUrl, entrepreneurship)
+    }
+    //modifcar  eliminar consulta x id
+  
+    // Método para actualizar un Entrepreneurship
+    updateEntrepreneurship(entrepreneurship: Entrepreneurship): Observable<Entrepreneurship> {
+      // Se hace una solicitud PUT para actualizar el dueño por su id
+      return this.http.put<Entrepreneurship>(`${this.apiUrl}/${entrepreneurship.idEntrepreneurship}`, entrepreneurship);
+    }
+  
+    // Eliminar Entrepreneurship
+    deleteEntrepreneurship(idEntrepreneurship: number): Observable<void> {
+      return this.http.delete<void>(`${this.apiUrl}/${idEntrepreneurship}`);
+    }
+
 }
